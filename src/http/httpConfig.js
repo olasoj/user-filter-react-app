17b86e0c22e9010bfe3be16cbdff3b68eefd2727
olasoj <olasojiige@gmail.com>
2021-09-14 06:47:31 +0100

    Configured http service

---
+++ src/http/httpConfig.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL
+axios.interceptors.response.use(null, (error) => {
+    const expectedErr =
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status < 500
+    if (!expectedErr) console.log("unexpected error")
+    return Promise.reject(error)
+})
+
+function setJwt(jwt) {
+    axios.defaults.headers.common["x-auth-token"] = jwt
+}
+
+const http = {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete,
+    setJwt,
+}
+export default http
